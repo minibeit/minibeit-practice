34460578e60bae45cd95bb4f43d57eb599289e25
donup3 <donup08@naver.com>
2021-07-28 11:17:06 +0900

    initial commit

---
+++ backend/src/main/java/com/miniprac/MiniPracApplication.java
@@ -0,0 +1,13 @@
+package com.miniprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniPracApplication.class, args);
+	}
+
+}
