23d80cc411c33452a9dc1d0fd38da2803cbbc688
rkdud007 <gayeongparkk@gmail.com>
2021-08-03 21:51:59 +0900

    feat: ExplainContent틀 (#5)

---
+++ frontend/src/components/ExplainPage/ExplainTitle/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import PExplainTitle from "./PExplainTitle";
+
+export default function ExplainTitle({ category }) {
+  return <PExplainTitle category={category} />;
+}
--- frontend/src/pages/ExplainPage/index.jsx
+++ frontend/src/pages/ExplainPage/index.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { ExplainContent, ExplainTitle } from "../../components/ExplainPage";
 
-export default function ExplainPage() {
-  return <div>explainpage</div>;
+export default function ExplainPage({ match }) {
+  const { category } = match.params;
+  console.log(category);
+
+  return (
+    <>
+      <ExplainTitle category={category} />
+      <ExplainContent category={category} />
+    </>
+  );
 }
