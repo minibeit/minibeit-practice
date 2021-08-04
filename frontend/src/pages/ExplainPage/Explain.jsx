5b4982d0ba2f15d9297bbdc3b4c6b66190630d31
seol <shs00421@gmail.com>
2021-08-04 15:48:17 +0900

    feat: explain페이지 작성 (#7)

---
+++ frontend/src/pages/ExplainPage/Explain.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+/* Components */
+import * as S from './style'
+
+export function RecruitExplain(){
+    return (
+        <S.ExplainBox>
+            <img src="http://placeimg.com/200/200/any"/>
+            <p>실험자 모집은 이렇게 하시면 됩니다.</p>
+        </S.ExplainBox>
+    )
+}
+
+export function ApplyExplain(){
+    return (
+        <S.ExplainBox>
+            <img src="http://placeimg.com/200/200/any"/>
+            <p>실험자 지원은 이렇게 하시면 됩니다.</p>
+        </S.ExplainBox>
+    )
+}
--- frontend/src/pages/ExplainPage/index.jsx
+++ frontend/src/pages/ExplainPage/index.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 /* Components */
 import NavBar from '../../components/NavBar/NavBar';
+import { RecruitExplain, ApplyExplain } from './Explain'
 
 export default function ExplainPage({match}) {
   return (
       <>
         <NavBar/>
-        <div>{match.params.category} explainpage</div>
+        {
+          match.params.category == 'recurit'
+          ? <RecruitExplain/>
+          : <ApplyExplain/>
+        }
+        <Link to='/applyHome'><button>minibeit 시작하기</button></Link>
       </>
     );
 }
