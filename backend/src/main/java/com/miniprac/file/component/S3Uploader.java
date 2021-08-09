4d1d38831a3d9dd84db2abba96d9eff0f910eacd
donup3 <donup08@naver.com>
2021-08-09 14:56:06 +0900

    feat: 게시글 파일 업로드 수정, 설정 변경(#15)
    AWS 환경변수 사용 ->  yml 파일 사용

--- backend/src/main/java/com/miniprac/file/component/S3Uploader.java
+++ backend/src/main/java/com/miniprac/file/component/S3Uploader.java
@@ -1,29 +1,37 @@
 package com.miniprac.file.component;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.miniprac.common.dto.SavedFile;
 import com.miniprac.file.domain.FileServer;
 import com.miniprac.file.domain.FileType;
 import com.miniprac.file.service.exception.S3FileUploadException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-@Service
+@Component
 @RequiredArgsConstructor
 public class S3Uploader {
-    private final S3Client s3client;
-    private final S3Props storageProps;
+    private final AmazonS3Client s3Client;
+    @Value("${cloud.aws.s3.bucket}")
+    private String s3Bucket;
+    @Value("${cloud.aws.s3.public}")
+    private String s3Public;
+
 
     public List<SavedFile> uploadFileList(List<MultipartFile> files) {
         return files.stream().map(this::upload).collect(Collectors.toList());
@@ -35,20 +43,15 @@ public class S3Uploader {
                 .filter(s -> s.contains("."))
                 .map(s -> s.substring(originalName.lastIndexOf(".") + 1))
                 .orElse(null);
-
         String s3FileName = Optional.ofNullable(RandomStringBuilder.generateAlphaNumeric(60)).orElseThrow() + "." + extension;
-        String publicUrl = storageProps.getS3Public() + "/" + s3FileName;
+        String publicUrl = s3Public + "/" + s3FileName;
 
         boolean isImage = this.isImage(extension);
         Integer width = null;
         Integer height = null;
         try {
-            PutObjectResponse response = s3client.putObject(
-                    PutObjectRequest.builder()
-                            .key(s3FileName)
-                            .bucket(storageProps.getS3Bucket())
-                            .build(),
-                    RequestBody.fromBytes(file.getBytes()));
+            File uploadFile = convert(file).orElseThrow();
+            upload(uploadFile, s3FileName);
             if (isImage) {
                 BufferedImage image = ImageIO.read(file.getInputStream());
                 width = image.getWidth();
@@ -73,9 +76,34 @@ public class S3Uploader {
         return savedFileBuilder.fileType(FileType.FILE).build();
     }
 
+    private void upload(File uploadFile, String fileName) {
+        putS3(uploadFile, fileName);
+        removeNewFile(uploadFile);
+    }
+
+    private void putS3(File uploadFile, String fileName) {
+        s3Client.putObject(new PutObjectRequest(s3Bucket, fileName, uploadFile).withCannedAcl(CannedAccessControlList.PublicRead));
+    }
+
+    private void removeNewFile(File targetFile) {
+        targetFile.delete();
+    }
+
     private boolean isImage(String extension) {
         return Optional.ofNullable(extension)
                 .map(s -> s.toLowerCase().matches("png|jpeg|jpg|bmp|gif|svg"))
                 .orElse(false);
+    }
+
+    private Optional<File> convert(MultipartFile file) throws IOException {
+        File convertFile = new File(Objects.requireNonNull(file.getOriginalFilename()));
+        if (convertFile.createNewFile()) {
+            try (FileOutputStream fos = new FileOutputStream(convertFile)) {
+                fos.write(file.getBytes());
+            }
+            return Optional.of(convertFile);
+        }
+
+        return Optional.empty();
     }
 }
