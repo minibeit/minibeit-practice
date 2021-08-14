42fbb8b0c8eeb421f11c800ba061386f573fa6dd
rkdud007 <gayeongparkk@gmail.com>
2021-08-14 14:47:08 +0900

    feat:  refresh (#5)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,10 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import axios from "axios";
 
+axios.defaults.withCredentials = true;
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
   document.getElementById('root'),
 );
-axios.defaults.withCredentials = true;
